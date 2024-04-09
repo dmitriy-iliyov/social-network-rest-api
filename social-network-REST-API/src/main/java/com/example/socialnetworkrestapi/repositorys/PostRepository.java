33a9112b350341dd951d34e70d5bf6dc4da638d1
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-04-09 05:07:14 +0300

    upd repository

--- social-network-REST-API/src/main/java/com/example/socialnetworkrestapi/repositorys/PostRepository.java
+++ social-network-REST-API/src/main/java/com/example/socialnetworkrestapi/repositorys/PostRepository.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends CrudRepository<PostEntity, Long> {
 
-    Iterable<PostEntity> findAllByUserIdOrUserNameOrCategoryIdOrCategoryName(Long user_id, String user_name, Long category_id, String category_name);
+    Iterable<PostEntity> findAllByUserIdOrUserNameOrCategoryIdOrCategoryName(
+            Long user_id,
+            String user_name,
+            Long category_id,
+            String category_name);
 
 }
