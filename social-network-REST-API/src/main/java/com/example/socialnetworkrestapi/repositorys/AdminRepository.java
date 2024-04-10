ff5fa7bde3a18b6f69585fab6713ece419de59e4
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-04-10 08:30:46 +0300

    repo chenges

--- social-network-REST-API/src/main/java/com/example/socialnetworkrestapi/repositorys/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.socialnetworkrestapi.repositorys;
-
-import com.example.socialnetworkrestapi.models.entitys.AdminEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminRepository extends CrudRepository<AdminEntity, Long> {
-
-    void deleteByPassword(String password);
-}
