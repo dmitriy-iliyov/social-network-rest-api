204e5710833790025afd4894f178a5f0aeb95cc3
Dima Iliov <dmitriy.iliyov@gmail.com>
2024-04-13 10:53:17 +0300

    home controller test added

--- social-network-REST-API/src/main/java/com/example/socialnetworkrestapi/controllers/HomeController.java
+++ social-network-REST-API/src/main/java/com/example/socialnetworkrestapi/controllers/HomeController.java
@@ -1,15 +1,19 @@
 package com.example.socialnetworkrestapi.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
     public String homePage(){
         return "home_page";
     }
